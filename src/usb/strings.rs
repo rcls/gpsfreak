6608ca4e186876c0538def4f8da32f4ba498d221
Ralph Loader <ralph1loader@gmail.com>
2025-12-06 14:39:08 +1300

    Turn the string table generation into a big macro

--- src/usb/strings.rs
+++ src/usb/strings.rs
@@ -1,22 +1,22 @@
+
 use super::types::SetupResult;
 
 mod string_table;
 
-pub use string_table::string_index;
+crate::define_usb_strings!{}
 
 type Offset = u8;
-
-const STRING_LIST: [&str; 8] = [
+pub const STRING_LIST: [&str; 8] = [
     "\u{0409}", // Languages.
     "Ralph", "GPS Freak", "Device Configuration",
     "CDC", "CDC DATA interface", "Device Control", "DFU",
 ];
 
-pub const IDX_SERIAL_NUMBER: u8 = string_table::NUM_STRINGS as u8;
+pub const IDX_SERIAL_NUMBER: u8 = NUM_STRINGS as u8;
 
 pub fn get_descriptor(idx: u8) -> SetupResult {
     if idx != IDX_SERIAL_NUMBER {
-        return string_table::get_descriptor(idx);
+        return _get_descriptor(idx);
     }
     // Special case.
     let data = crate::command::USB_NAME.as_ref();
--- src/usb/strings/string_table.rs
+++ src/usb/strings/string_table.rs
@@ -1,63 +1,66 @@
 
-use super::{STRING_LIST, Offset};
-use crate::usb::SetupResult;
 
-pub const fn string_index(s: &str) -> u8 {
-    let mut i = 0;
-    loop {
-        if STRING_LIST[i as usize] == s {
-            return i;
+#[macro_export]
+macro_rules! define_usb_strings{
+    {} => {
+        pub const fn string_index(s: &str) -> u8 {
+            let mut i = 0;
+            loop {
+                if STRING_LIST[i as usize] == s {
+                    return i;
+                }
+                i += 1;
+            }
         }
-        i += 1;
+
+        pub const NUM_STRINGS: usize = STRING_LIST.len();
+
+        const LENGTHS: [usize; NUM_STRINGS] = konst::iter::collect_const!(
+            usize => STRING_LIST, map($crate::usb::strings::string_table::str_utf16_count));
+
+        const TOTAL_LENGTH: usize = OFFSETS[NUM_STRINGS - 1] as usize
+            + LENGTHS[NUM_STRINGS - 1] + NUM_STRINGS;
+
+        static OFFSETS: [Offset; NUM_STRINGS] = {
+            let mut o = [0; _];
+            let mut p = 0;
+            let mut i = 0;
+            while i < NUM_STRINGS {
+                o[i] = p;
+                p += LENGTHS[i] as Offset + 1;
+                i += 1;
+            }
+            o
+        };
+
+        static DATA: [u16; TOTAL_LENGTH] = {
+            let mut d = [0; _];
+            let mut i = 0;
+            while i < NUM_STRINGS {
+                let start = OFFSETS[i] as usize;
+                let end = start + LENGTHS[i] as usize;
+                // Byte count (length*2+2), Descriptor type (3), as LE word.
+                d[start] = LENGTHS[i] as u16 * 2 + 2 + 0x300;
+                $crate::usb::strings::string_table::str_to_utf16_inplace(&mut d[start + 1 ..= end], STRING_LIST[i]);
+                i += 1;
+            }
+            d
+        };
+
+        pub fn _get_descriptor(idx: u8) -> SetupResult {
+            if idx as usize >= NUM_STRINGS {
+                return SetupResult::error();
+            }
+            let offset = OFFSETS[idx as usize] as usize;
+            let len = DATA[offset] as usize & 255;
+            let data: &[u8] = unsafe{core::slice::from_raw_parts(
+                &DATA[offset] as *const u16 as *const u8, len)};
+            SetupResult::Tx(data)
+        }
     }
 }
 
-pub const NUM_STRINGS: usize = STRING_LIST.len();
-
-const LENGTHS: [usize; NUM_STRINGS] = konst::iter::collect_const!(
-    usize => STRING_LIST, map(str_utf16_count));
-
-const TOTAL_LENGTH: usize = OFFSETS[NUM_STRINGS - 1] as usize
-    + LENGTHS[NUM_STRINGS - 1] + NUM_STRINGS;
-
-static OFFSETS: [Offset; NUM_STRINGS] = {
-    let mut o = [0; _];
-    let mut p = 0;
-    let mut i = 0;
-    while i < NUM_STRINGS {
-        o[i] = p;
-        p += LENGTHS[i] as Offset + 1;
-        i += 1;
-    }
-    o
-};
-
-static DATA: [u16; TOTAL_LENGTH] = {
-    let mut d = [0; _];
-    let mut i = 0;
-    while i < NUM_STRINGS {
-        let start = OFFSETS[i] as usize;
-        let end = start + LENGTHS[i] as usize;
-        // Byte count (length*2+2), Descriptor type (3), as LE word.
-        d[start] = LENGTHS[i] as u16 * 2 + 2 + 0x300;
-        str_to_utf16_inplace(&mut d[start + 1 ..= end], STRING_LIST[i]);
-        i += 1;
-    }
-    d
-};
-
-pub fn get_descriptor(idx: u8) -> SetupResult {
-    if idx as usize >= NUM_STRINGS {
-        return SetupResult::error();
-    }
-    let offset = OFFSETS[idx as usize] as usize;
-    let len = DATA[offset] as usize & 255;
-    let data: &[u8] = unsafe{core::slice::from_raw_parts(
-        &DATA[offset] as *const u16 as *const u8, len)};
-    SetupResult::Tx(data)
-}
-
-const fn str_utf16_count(s: &str) -> usize {
+pub const fn str_utf16_count(s: &str) -> usize {
     let mut i = konst::string::chars(s);
     let mut n = 0;
     while let Some(c) = i.next() {
@@ -66,7 +69,7 @@ const fn str_utf16_count(s: &str) -> usize {
     n
 }
 
-const fn str_to_utf16_inplace(u: &mut [u16], s: &str) {
+pub const fn str_to_utf16_inplace(u: &mut [u16], s: &str) {
     let mut i: usize = 0;
     let mut j = konst::string::chars(s);
     while let Some(x) = j.next() {
